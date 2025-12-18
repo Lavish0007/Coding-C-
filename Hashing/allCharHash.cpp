584aafcd8fa355eebb4454d25d25a7be8e99bdaa
Lavish0007 <patellavish.9336@gmail.com>
2025-12-18 15:15:27 +0530

    final

---
+++ Hashing/allCharHash.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std; //hashing for all characters
+int main(){
+     
+
+    return 0;
+}
---
+++ Hashing/charHash.cpp
@@ -0,0 +1,20 @@
+//Hashing for lowercase only
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int hash[26]={0};
+    for(int i=0;i<s.size();i++){
+        hash[s[i]-'a']+=1;
+    }
+    int t;
+    cin>>t;
+    while(t--){
+        char c;
+        cin>>c;
+        cout<<hash[c-'a']<<endl;
+    }
+
+}
