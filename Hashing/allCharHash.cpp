637c3cc8e5fd2e7b4f67024d27d5de293407ecfd
Lavish0007 <patellavish.9336@gmail.com>
2025-12-18 15:18:27 +0530

    all char

--- Hashing/allCharHash.cpp
+++ Hashing/allCharHash.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std; //hashing for all characters
 int main(){
+    string s;
+    cin>>s;
+    int hash[256]={0}; // just take a 256 size array and now we dont need to minus 'a' from thee char 
+    for(int i=0;i<s.size();i++){
+        hash[s[i]]+=1;
+    }
+    int t;
+    cin>>t;
+    while(t--){
+        char c;
+        cin>>c;
+        cout<<hash[c]<<endl;
+    }
      
 
     return 0;
