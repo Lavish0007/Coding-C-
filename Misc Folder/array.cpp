1c0ac0eb5ba914e95bad912102b8dd531c6ff072
Lavish0007 <patellavish.9336@gmail.com>
2025-12-21 22:22:34 +0530

    all char

--- Misc Folder/array.cpp
+++ Misc Folder/array.cpp
@@ -1,7 +1,18 @@
  #include<bits/stdc++.h>
  using namespace std;
  int main(){
-      
- 
+    vector<int>arr={1,2,3,4,5,6};
+    for(int i=0;i<arr.size();i++){
+        for(int j=i;j<arr.size();j++){
+            int sum=0;
+            for(int k=i;k<=j;k++){
+                sum+=arr[k];
+                cout<<arr[k]<<" ";
+            }
+            cout<<" -> "<<sum<<endl;
+        }
+        cout<<endl;
+        
+    }
      return 0;
  }
