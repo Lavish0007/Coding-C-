30c90bf0862bb59f13774c66475147795f414084
Lavish0007 <patellavish.9336@gmail.com>
2025-12-23 12:17:16 +0530

    finally done

--- Misc Folder/array.cpp
+++ Misc Folder/array.cpp
@@ -3,26 +3,24 @@
  int main(){
     vector<int>arr={1,2,3,1,1,1,1,3,3};
     int k=6,len=0;
-    int i=0,j=1,sum=arr[i];
+    int i=0,j=0,sum=arr[i];
     while(j<arr.size()){
-        if(sum<k){
-            sum+=arr[j];
-            j++;
-        }
-        else if(sum==k){
-            len=max(len,j-i+1);
-            j++;
-            sum+=arr[j];
-        }
-        else{
+        while(i<=j && sum>k){
             sum-=arr[i];
             i++;
         }
+        if(sum==k){
+            len=max(len,j-i+1);
+        }
+        j++;
+        if (j<arr.size()) sum+=arr[j];
+
 
     }
     
     cout<<len;
      return 0;
+    
  }
 
  // Brute
