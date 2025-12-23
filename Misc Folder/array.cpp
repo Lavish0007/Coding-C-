edf5f6a6c130dba9b01c90c5d3c269c5e4da7705
Lavish0007 <patellavish.9336@gmail.com>
2025-12-23 12:04:50 +0530

    its this

--- Misc Folder/array.cpp
+++ Misc Folder/array.cpp
@@ -1,18 +1,28 @@
  #include<bits/stdc++.h>
  using namespace std;
  int main(){
-    vector<int>arr={1,2,3,4,5,6};
-    for(int i=0;i<arr.size();i++){
-        for(int j=i;j<arr.size();j++){
-            int sum=0;
-            for(int k=i;k<=j;k++){
-                sum+=arr[k];
-                cout<<arr[k]<<" ";
-            }
-            cout<<" -> "<<sum<<endl;
+    vector<int>arr={1,2,3,1,1,1,1,3,3};
+    int k=6,len=0;
+    int i=0,j=1,sum=arr[i];
+    while(j<arr.size()){
+        if(sum<k){
+            sum+=arr[j];
+            j++;
         }
-        cout<<endl;
-        
+        else if(sum==k){
+            len=max(len,j-i+1);
+            j++;
+            sum+=arr[j];
+        }
+        else{
+            sum-=arr[i];
+            i++;
+        }
+
     }
+    
+    cout<<len;
      return 0;
  }
+
+ // Brute
