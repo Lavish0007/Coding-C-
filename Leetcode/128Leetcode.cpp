cda657fffa1fc2de920ffad29f3eca70c7f28343
Lavish0007 <patellavish.9336@gmail.com>
2026-01-18 19:09:22 +0530

    leetcodes

---
+++ Leetcode/128Leetcode.cpp
@@ -0,0 +1,32 @@
+//128. Longest Consecutive Sequence
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int longestConsecutive(vector<int>& nums) {
+    int count=0,lastSmall=INT_MIN,longest=1;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n==0) return 0;
+        for(int i=0;i<n;i++){
+            
+            if(nums[i]-1==lastSmall){
+                count++;
+                lastSmall=nums[i];
+            }
+            else if(nums[i]!=lastSmall){
+                count=1;
+                lastSmall=nums[i];
+            }
+            longest=max(longest,count);
+        }
+        return longest;
+        
+    }
+int main(){
+    vector<int>nums={100,4,200,1,3,2};
+    cout<<longestConsecutive(nums);
+
+
+    
+}
