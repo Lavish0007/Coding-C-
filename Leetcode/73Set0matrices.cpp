f33ee8f40dd2804aa4b187bf9032f86cd062c358
Lavish0007 <patellavish.9336@gmail.com>
2026-01-19 16:28:26 +0530

    new changes

--- Leetcode/73Set0matrices.cpp
+++ Leetcode/73Set0matrices.cpp
@@ -1,1 +1,58 @@
 //better solution
+#include <bits/stdc++.h>
+using namespace std;
+
+void setZeroes(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    vector<int> row(n, 0);
+    vector<int> col(m, 0);
+
+    // Step 1: Mark rows and columns
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+
+    // Step 2: Set zeroes
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 1, 1},
+        {1, 0, 1},
+        {1, 1, 1}
+    };
+
+    cout << "Original Matrix:\n";
+    for (auto &row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    setZeroes(matrix);
+
+    cout << "\nMatrix After setZeroes:\n";
+    for (auto &row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
