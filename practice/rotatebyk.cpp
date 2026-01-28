41eb11eecc292e5736e27771f0ac43184a06e196
Lavish0007 <patellavish.9336@gmail.com>
2026-01-28 07:31:59 +0530

    practice

---
+++ practice/rotatebyk.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int k=3;
+    vector<int>v={1,2,3,4,5,6,7}; 
+    int n=v.size();
+    k=k%n;
+    vector<int>ans(n,0);
+    for(int i=0;i<n-k;i++){
+        ans[i]=v[i+k];
+    }
+    int a=0;
+    for(int i=n-k;i<n;i++){
+        ans[i]=v[a];
+        a++;
+    }
+    for(int val:ans){
+        cout<<val<<" ";
+    }
+    return 0;
+}
