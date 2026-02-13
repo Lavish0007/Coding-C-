f21e743e3b4a3b73e0d720d468c3a08e879f5342
Lavish0007 <patellavish.9336@gmail.com>
2026-02-13 05:51:53 +0530

    string

--- practice/mao_string.cpp
+++ practice/mao_string.cpp
@@ -3,8 +3,9 @@ using namespace std;
 int main(){
     unordered_map<char,int>mp; 
     mp.insert({'a',1});
+    mp.insert({'A',1});
     for(auto it:mp){
-        cout<<it.first<<" "<<it.second;
+        cout<<it.first-32<<" ";
     }
     return 0;
 }
