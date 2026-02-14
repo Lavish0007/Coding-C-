98bfb01cf227bd9b364834e778b6ebf0198dcfc8
Lavish0007 <patellavish.9336@gmail.com>
2026-02-14 17:02:04 +0530

    string

--- practice/map.cpp
+++ practice/map.cpp
@@ -1,9 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s="eOEe";
-    sort(s.begin(),s.end());
-    cout<<s;
+    string s="aabcbbca";
+    unordered_map<char,int>mp;
+    int mx=-1,smx=-1;
+    for(char c:s){
+        mp[c]++;
+        mx=max(mx,mp[c]);
+        if(mp[c]!=mx){
+            smx=max(smx,mp[c]);
+        }
+    }
+    cout<<mx<<" "<<smx;
+    for(auto &it:mp){
+        it.second-=smx;
+        
+    }
+    string ans="";
+    for(auto it:mp){
+        if(it.second>0){
+            ans.insert(0,it.second,it.first);
+        }        
+    }
+    cout<<ans;
     
 
     return 0;
