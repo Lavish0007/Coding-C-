ecb7cc849f24ec747479f2fcb380a6baa59b218a
Lavish0007 <patellavish.9336@gmail.com>
2026-03-19 08:42:04 +0530

    dsa sliding window

--- Sliding window/2.cpp
+++ Sliding window/2.cpp
@@ -3,7 +3,32 @@ using namespace std;
 int main(){
     vector<int>vec={-1,0,-2,3,-4,6,7,5,-1};
     vector<int>ans;
-    
+    queue<int>q;
+    int i=0,j=0,n=vec.size(),k=3;
+    while(j<n){
+        // Calculation
+        if(vec[j]<0) q.push(vec[j]);
+
+        //less size of window
+        if(j-i+1<k) j++;
+
+        //Window size met
+        else if(j-i+1==k){
+
+            //deduce answer
+            if(q.size()==0) ans.push_back(0);
+            else{
+                ans.push_back(q.front());
+            }
+
+            // slide window 
+            
+            if(vec[i]==q.front()) q.pop();
+            i++;j++;
+        }
+
+
+    }
     for(int val:ans) cout<<val<<" ";
     
     return 0;
